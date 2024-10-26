be6956d5bb54bcbe29e97db07f00e585869e3fa7
menoo-o <menosuper6@gmail.com>
2024-10-26 16:13:53 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+React JS Form Validation | Axios User Registration Form Submit
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Register from './components/Register';
+
+export default function App() {
+  return (
+    <main className='App'>
+      <Register /> 
+    </main>
+  )
+}
